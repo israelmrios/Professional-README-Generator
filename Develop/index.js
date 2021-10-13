30347b9433784334a36794ebf80d4f03ebf0bbcd
Israel M. Rios <israelm.riosjr@gmail.com>
2021-10-13 00:26:09 -0700

    Added writeToFile Fuction

--- Develop/index.js
+++ Develop/index.js
@@ -10,56 +10,60 @@ const questions = [
         type: "input",
         name: "title",
         message: "What is your projects title?",
-    }
+    },
     {
         type: "input",
         name: "description",
         message: "Provide a short description explaining your project?"
-    }
+    },
     {
         type: "input",
         name: "installation",
         message: "What are the steps required to install your project?"
-    }
+    },
     {
         type: "input",
         name: "usage",
         message: "Provide instructions and/or short description on how to use this application?"
-    }
+    },
     {
         type: "list",
         name: "license",
         message: "Chose a license?",
-        choices: [Mozilla Public License 2.0, Apache License 2.0, MIT License],
-    }
+        choices: ["Mozilla Public License 2.0", "Apache License 2.0", "MIT License"],
+    },
     {
         type: "input",
         name: "contributing",
-        message: "contribution guidelines"
-    }
+        message: "contribution guidelines",
+    },
     {
         type: "input",
         name: "tests",
         message: "test instructions",
-    }
+    },
     {
         type: "input",
         name: "username",
         message: "Please enter your GitHub username.",
-    }
+    },
     {
         type: "input",
         name: "email",
         message: "Please enter your email where you can be contacted at for any questions reguarding this project.",
-    }
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("README.md", generateMarkdown(data));
+    })
 }
 
 // Function call to initialize app
